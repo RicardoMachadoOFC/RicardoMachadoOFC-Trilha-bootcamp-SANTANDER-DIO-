ec805ef776531531ddcb6d9f198d3901e2d23c59
Ricardo Machado <cado.machado@hotmail.com>
2024-05-02 23:50:04 -0300

    projeto completo televisão

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -1,5 +1,32 @@
 public class SmartTv {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
+        boolean ligada = false;
+        int canal = 1;
+        int volume = 25;
+
+        public void aumentarVolume(){
+            volume++;
+            System.out.println("Aumentando o volume para: " + volume);
+        }
+        public void abaixarVolume(){
+            volume--;
+            System.out.println("abaixando o volume para: " + volume);
+        }
+        public void ligar(){
+            ligada=true;
+        }
+        public void desligar(){
+            ligada=false;  
+        }
+        public void aumentarCanal(){
+            canal++;
+        }
+        public void diminuirCanal(){
+            canal--;
+        }
+        public void mudarCanal( int novoCanal){
+            canal = novoCanal;
+        }
+
+        }
+
+    
