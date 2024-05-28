53d8acc9746e921b1666eab4ef2cee306c1b4bd0
RicardoMachadoOFC <cado.machado@hotmail.com>
2024-05-28 19:59:34 -0300

    finalização de projeto

--- PraticandoLógica/ControleSimplesDeSaques.java
+++ PraticandoLógica/ControleSimplesDeSaques.java
@@ -8,24 +8,25 @@ public class ControleSimplesDeSaques {
         System.out.println("Digite o limite diário de saque: ");
         double limiteDiario = scanner.nextDouble();
 
-        // Loop for para iterar sobre os saques
+       
         for (int i = 1; ; i++) {
             System.out.println("Digite o valor do saque (ou 0 para encerrar): ");
             double valorSaque = scanner.nextDouble();
 
             if (valorSaque == 0) {
                 System.out.println("Transações encerradas.");
-                break; // Encerra o loop
+                    break; 
             } else if (valorSaque > limiteDiario) {
                 System.out.println("Limite diário de saque atingido. Transações encerradas.");
-                break; // Encerra o loop
+                break; 
             } else {
                 limiteDiario -= valorSaque;
                 System.out.println("Saque realizado. Limite restante: " + limiteDiario);
             }
         }
 
-        // Fechar o scanner para evitar vazamentos de recursos
+
+       
         scanner.close();
     }
 }
