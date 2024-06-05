dac071b4c754770289267a6bc8fe1d7e58d41230
RicardoMachadoOFC <cado.machado@hotmail.com>
2024-06-05 20:04:20 -0300

    BOOTCAMP FEITO COM POO

--- PraticandoLógica/ControleSimplesDeSaques.java
+++ PraticandoLógica/ControleSimplesDeSaques.java
@@ -1,3 +1,5 @@
+package PraticandoLógica;
+
 import java.util.Scanner;
 
 public class ControleSimplesDeSaques {
@@ -8,25 +10,22 @@ public class ControleSimplesDeSaques {
         System.out.println("Digite o limite diário de saque: ");
         double limiteDiario = scanner.nextDouble();
 
-       
-        for (int i = 1; ; i++) {
+        for (int i = 1;; i++) {
             System.out.println("Digite o valor do saque (ou 0 para encerrar): ");
             double valorSaque = scanner.nextDouble();
 
             if (valorSaque == 0) {
                 System.out.println("Transações encerradas.");
-                    break; 
+                break;
             } else if (valorSaque > limiteDiario) {
                 System.out.println("Limite diário de saque atingido. Transações encerradas.");
-                break; 
+                break;
             } else {
                 limiteDiario -= valorSaque;
                 System.out.println("Saque realizado. Limite restante: " + limiteDiario);
             }
         }
 
-
-       
         scanner.close();
     }
 }
