7a0b4c8b7410d16a5a52a508ce124129366fee41
RicardoMachadoOFC <cado.machado@hotmail.com>
2024-06-11 19:21:45 -0300

    atualização de projetos

--- lab-padroes-projeto-java/src/one/digitalinnovation/gof/SingletonLazy.java
+++ lab-padroes-projeto-java/src/one/digitalinnovation/gof/SingletonLazy.java
@@ -1,2 +1,16 @@
-package one.digitalinnovation.gof;public class SingletonLazy {
+
+public class SingletonLazy {
+
+    private static SingletonLazy instancia;
+
+    private SingletonLazy() {
+        super();
+    }
+
+    public static SingletonLazy getInstancia() {
+        if (instancia == null) {
+            instancia = new SingletonLazy();
+        }
+        return instancia;
+    }
 }
